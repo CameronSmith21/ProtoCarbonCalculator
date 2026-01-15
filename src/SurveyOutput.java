89463f09cd36f01813fe94dd1a6e5b35736d3ba9
CameronSmith21 <122478503+CameronSmith21@users.noreply.github.com>
2026-01-15 09:25:49 -0600

    Added SurveyOutput command

---
+++ src/SurveyOutput.java
@@ -0,0 +1,20 @@
+/**
+ * JavaDocs go here
+ * 
+ */
+
+public class SurveyOutput {
+    //Put all instance variables here
+    public double dailyDrivingHours;
+
+    //Constructor
+    //Put any values that you want to enter upon object creation here - e.g. dailyDrivingHours
+    public SurveyOutput(double dailyDrivingHours) {
+        this.dailyDrivingHours = dailyDrivingHours;
+    }
+
+    //DailyDrivingHours getter command
+    public double getDailyDrivingHours () {
+        return dailyDrivingHours;
+    }
+}
